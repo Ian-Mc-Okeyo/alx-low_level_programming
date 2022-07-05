e1476190255ff429947c833866295dc1e5916d0a
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-05 21:55:08 +0300

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main -Entry Point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
 	char sentence[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(sentence);
+
 	return (0);
 }
