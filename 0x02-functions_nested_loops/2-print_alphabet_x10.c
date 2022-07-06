f0623ceb2090f7ec237d2b9fa246a24a6a30c931
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-06 11:30:14 +0300

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all the alphabet in small letters 10 times
+ * users only putchar
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i, k, length;
+
+	length = sizeof(alphabet) / sizeof(char);
+
+	for (k = 0; k < 10; k++)
+		for (i = 0; i < length; i++)
+			_putchar(alphabet[i]);
+		_putchar('\n');
+}
