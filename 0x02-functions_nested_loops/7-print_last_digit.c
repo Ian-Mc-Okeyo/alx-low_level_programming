46e3781cccea723ed891b30c1c73b5b3f45af918
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-06 16:03:02 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - returns the last digit of a number
+ *
+ * @n: integer to be checked
+ *
+ * Return: absolute value of an integer
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
