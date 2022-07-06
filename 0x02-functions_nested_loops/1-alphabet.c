cf5b9d8cb522ecc5886b74110172711b4fc41cbc
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-06 11:00:34 +0300

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+ * main - entry point for the program
+ *
+ * Return: 0 if successful
+ *
+ *
+ **/
+ int main(void)
+{
+	_putchar(print_alphabet());
+	
+	_putchar('\n');
+
+	return (0);
+}
