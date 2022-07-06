85c25c6231af6f216dcf764727f338bcbfb136d2
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-06 13:13:20 +0300

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_abs - computes the absolute number
+ *
+ * @n: the number to be checked
+ *
+ * Return: absolute of the given numnber
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
