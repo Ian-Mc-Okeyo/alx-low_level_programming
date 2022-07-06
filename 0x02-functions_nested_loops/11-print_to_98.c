16b439376f6bc5baab3eb87d8b72b0d90ed7eed9
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-06 20:12:52 +0300

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,9 +18,9 @@ void print_to_98(int n)
 	for (i = n; i <= 98; i++)
 	{
 		if (n == 98)
-			printf("%d", &i);
+			printf("%d", i);
 		else
-			printf("%d, ", &i);
+			printf("%d, ", i);
 	}
 	_putchar('\n');
 
