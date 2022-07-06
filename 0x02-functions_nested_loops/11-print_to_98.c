43c0159e0c71dcfcc664f9078030f3ebb6cbc2c0
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-06 22:51:50 +0300

    natural

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,56 +13,30 @@ void print_to_98(int n)
 {
 	int i;
 
-	scanf("%d", &n);
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i == 98)
+			_putchar(i + '0');
+			if (i != 98)
 			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				if (i > 9)
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-				}
-				else
-				{
-					_putchar(i + '0');
-				}
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
+		_putchar('\n');
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
-			if (i == 98)
+			_putchar(i + '0');
+			if (i != 98)
 			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				if (i > 9)
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-				}
-				else
-				{
-					_putchar(i + '0');
-				}
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
