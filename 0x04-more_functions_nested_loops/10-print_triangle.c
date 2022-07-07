498bb4d5e9eab9c405e25351e68844b6a5631656
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-07 20:40:00 +0300

    Hashing a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,12 +17,12 @@ void print_triangle(int size)
 			{
 				_putchar(' ');
 			}
-			for (j = 0; j < i; j++)
+			for (j = 0; j <= i; j++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 	{
