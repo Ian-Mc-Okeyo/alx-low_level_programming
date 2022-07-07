ce2172929f8ced9c07b4dc678a6c51feb54baa36
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-07 20:23:35 +0300

    Hashing a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle in #
+ * @size: the size of the traingle
+ *
+ * Returns: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
