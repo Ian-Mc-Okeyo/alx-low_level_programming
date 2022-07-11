24d8e5dc04fb96024cd0aa462fc1d2bc21be4e8d
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-11 21:13:28 +0300

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - A function to find the length of a string
+ * @s: the string to find its length
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	length = sizeof(*s) / sizeof(char);
+
+	return (length);
+}
