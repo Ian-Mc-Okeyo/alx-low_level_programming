0007aeff77b18b6eacd2e27330fde3d1ad79caac
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-12 22:50:41 +0300

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints the last half of a string
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, length;
+
+	while (*(str + length) != '\0')
+		length++;
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; i < length; i++)
+			_putchar(*(str + i));
+	}
+	else
+	{
+		for (i = (length - 1) / 2; i < length; i++)
+			_putchar(*(str + i));
+	}
+}
