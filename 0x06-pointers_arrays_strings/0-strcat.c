6a1b3d585bf4dc5c693b30cbf6db3923a9fa2974
Ian-Mc-Okeyo <ianokeyo14559@gmail.com>
2022-07-14 22:42:13 +0300

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcat- prints all the alphabet in small letters
+ * @dest: string to be conccatenated to
+ * @src: soure string
+ *
+ * Return: string pinter
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, length, newLength;
+
+	length = 0;
+
+	while (*(dest + length) != '\0')
+		length++;
+
+	newLength = length;
+
+	while (*(dest + length) != '\0')
+		newLength++;
+
+	j = 0;
+
+	for (i = length, i < newLength; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	return (*dest);
+}
